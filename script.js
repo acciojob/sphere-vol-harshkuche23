182f3b60386ac80b317d98d8c9e71d6161f97321
harshkuche23 <harshkuche23@gmail.com>
2026-01-21 23:40:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function volume_sphere() {
-    //Write your code here
-  
-} 
+    event.preventDefault();
+
+    const radiusValue = document.getElementById("radius").value;
+    const radius = parseFloat(radiusValue);
+
+    let volume;
+
+    if (isNaN(radius) || radius < 0) {
+        volume = NaN;
+    } else {
+        volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+        volume = volume.toFixed(4);
+    }
+
+    document.getElementById("volume").value = volume;
+}
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
